6f8071ce045540ab225905703768da9beaebcad1
ahawkins94 <adamhawkins94@hotmail.co.uk>
2018-01-27 22:17:57 +0000

    Android Studio Setup
    Created an Android Studio project in a separate folder. Seeing if this way works with github free and easy.

---
+++ androidswipetest/app/src/main/java/com/example/adam/androidswipetest/SwipeTest.java
@@ -0,0 +1,13 @@
+package com.example.adam.androidswipetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SwipeTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipe_test);
+    }
+}
