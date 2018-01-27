df663052b28d5a58f168096617b8ffc82a399ef5
Kinxy <w.zacharyk@gmail.com>
2018-01-27 23:02:54 +0000

    basic control

--- androidswipetest/app/src/main/java/com/example/adam/androidswipetest/SwipeTest.java
+++ androidswipetest/app/src/main/java/com/example/adam/androidswipetest/SwipeTest.java
@@ -1,13 +1,37 @@
 package com.example.adam.androidswipetest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SwipeTest extends AppCompatActivity {
+
+    private Map<float, float> current;
+    private (Map<float, float>)[]
+
+
+    private double gradient;
+    private double[] gradient history;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swipe_test);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent even) {
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: Log.d("BEGIN", "BEGIN");
+
+
+            case MotionEvent.ACTION_MOVE: Log.d("MOVE", "MOVE");
+
+
+            case MotionEvent.ACTION_UP: Log.d("UP", "UP");
+        }
+    }
+
 }
