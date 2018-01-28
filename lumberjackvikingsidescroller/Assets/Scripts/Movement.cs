6758c0c0b9a97de7c184e220cac56f71809b4adf
SReed93 <s.sreed@outlook.com>
2018-01-28 15:14:46 +0000

    Movement satnight

--- lumberjackvikingsidescroller/Assets/Scripts/Movement.cs
+++ lumberjackvikingsidescroller/Assets/Scripts/Movement.cs
@@ -2,25 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class Movement : MonoBehaviour
+{
 
-    public int speed = 6;
-    private Vector2 location = new Vector2();
+    public float ballSpeed;
 
-	// Use this for initialization
-	void Start () {
-		speed = 200;
-        location.Set(1f,1f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKey(KeyCode.D)) {
-            Debug.Log("D");
-            Debug.Log(location.x);
-            location.x++;
-            Debug.Log(location.x);
-            
-        }                    
-	}
+    /* Update is called every frame inputs/moving objects*/
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+
+
+    }
+
 }
