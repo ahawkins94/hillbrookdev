088aa1899c99175ce3e8b0e2ab03679d8ef258e4
SReed93 <s.sreed2010@gmail.com>
2018-02-17 20:13:27 +0000

    level creation working to but need to set parameters

---
+++ levelcreation/LevelCreator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour {
+
+    public Vector3 originalPosition = new Vector3(0, 5, 0);
+    Vector3 backgroundPosition;
+    Vector3 levelPosition;
+    Vector3 cameraPosition;
+
+    public float cameraDistance = 5;
+    public float backgroundDistance = 10;
+    public float levelHeight = 0;
+
+    void Awake()
+    {
+        backgroundPosition = originalPosition;
+        levelPosition = originalPosition;
+        cameraPosition = originalPosition;
+        backgroundPosition.z -= backgroundDistance;
+        cameraPosition.z += cameraDistance;
+
+    }
+
+    void Start()
+    {
+        GameObject player = Instantiate(Resources.Load("Prefabs/Player/Player"), originalPosition, Quaternion.identity) as GameObject;
+
+        Vector2 playerSize = player.GetComponent<BoxCollider2D>().size;
+
+
+        //GameObject background = Instantiate(Resources.Load("Prefabs/Background/Background"), originalPosition, Quaternion.identity) as GameObject;
+
+        levelPosition.y -= levelHeight;
+        GameObject levelBlock = Instantiate(Resources.Load("Prefabs/LevelBlocks/Terrain"), levelPosition, Quaternion.identity) as GameObject;
+
+
+        GameObject camera = Instantiate(Resources.Load("Prefabs/Player/Camera"), cameraPosition, Quaternion.identity) as GameObject;
+    }
+
+}
