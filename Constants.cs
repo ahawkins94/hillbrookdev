0cf84aa984972280d3cf43211a236ae077a86b12
ahawkins94 <adamhawkins94@hotmail.co.uk>
2018-02-18 18:20:48 +0000

    added constants and began applying standardised unit

---
+++ Constants.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.hillbrookdev{ 
+
+public class Constants{
+
+    public const float STANDARD_UNIT = 0.1;
+
+     
+}
