5d058dbdca13c8186f4bf6bce2b72cbbecffebf7
ahawkins94 <adamhawkins94@hotmail.co.uk>
2018-03-25 15:52:45 +0100

    sorted out the namespace, forgot to change branches so committing straight to dev lmao woopsie

--- Functions/PathBlueprint.cs
+++ Functions/PathBlueprint.cs
@@ -1,63 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-using Assets.Scripts.hillbrookdev.Models.;
+using Assets.Scripts.hillbrookdev.Models.LevelVariables;
 
-public class PathBlueprint : MonoBehaviour
-=======
-
-public class PathBlueprint 
->>>>>>> 25b999e27054df8f8802b88b49892fb2fde560b1
+namespace Assets.Scripts.hillbrookdev
 {
+    public class PathBlueprint : MonoBehaviour
+    {
 
-    List<int> list = new List<int>();
-    int[] array = new int[3];
+        List<int> list = new List<int>();
+        int[] array = new int[3];
 
 
-    void scrapeLevelBlocks(string location) {
-<<<<<<< HEAD
-        // Loads all of the game objects in the levelblocks folder into an array
-        Object[] levelBlocks = Resources.LoadAll("Prefabs/LevelBlocks", typeof(GameObject));
+        void scrapeLevelBlocks(string location) {
+            // Loads all of the game objects in the levelblocks folder into an array
+            Object[] levelBlocks = Resources.LoadAll("Prefabs/LevelBlocks", typeof(GameObject));
 
-        // Creates an array of the variables in the array
-        LevelVariables[] levelBlockVarList = new LevelVariables[levelBlocks.Length];
-        for (int i = 0; i < levelBlocks.Length; i++) {
+            // Creates an array of the variables in the array
+            LevelVariables[] levelBlockVarList = new LevelVariables[levelBlocks.Length];
+            for (int i = 0; i < levelBlocks.Length; i++) {
 
-            GameObject current = (GameObject) Instantiate(levelBlocks[i], new Vector3(0, 0, 0), Quaternion.identity);
+                GameObject current = (GameObject) Instantiate(levelBlocks[i], new Vector3(0, 0, 0), Quaternion.identity);
 
-            // levelBlockVarList[i] = levelBlocks[i]
+                // levelBlockVarList[i] = levelBlocks[i]
 
-            Destroy(current);
+                Destroy(current);
+            }
         }
+
+        // contains an array of the level blocks names and randomly chooses one of them when ran 
+        public static void standardLevelBlocks(ref string randomStandardBlock){
+
+            string[] standardBlockList = {
+                "stone_flat1_15", 
+                "stone_flat2_15" 
+                };
+
+            int standardBlockListRange = standardBlockList.Length;
+            randomStandardBlock = standardBlockList[Random.Range(0, standardBlockListRange)];
+        }   
     }
-
-    // contains an array of the level blocks names and randomly chooses one of them when ran 
-=======
-        Object[] levelBlocks = Resources.LoadAll("Prefabs/LevelBlocks", typeof(GameObject));
-        LevelVariables[] levelBlockVarList = new LevelVariables[levelBlocks.Length];
-        for (int i = 0; i < levelBlocks.Length; i++) {
-
-            // GameObject current = (GameObject) Instantiate(levelBlocks[i], new Vector3(0, 0, 0), Quaternion.identity);
-
-            // levelBlockVarList[i] = levelBlocks[i]
-        }
-    }
-
-
->>>>>>> 25b999e27054df8f8802b88b49892fb2fde560b1
-    public static void standardLevelBlocks(ref string randomStandardBlock){
-
-        string[] standardBlockList = {
-            "stone_flat1_15", 
-            "stone_flat2_15" 
-            };
-
-        int standardBlockListRange = standardBlockList.Length;
-        randomStandardBlock = standardBlockList[Random.Range(0, standardBlockListRange)];
-    }   
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 25b999e27054df8f8802b88b49892fb2fde560b1
