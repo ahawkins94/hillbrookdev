fd35456ba2181aa35c4f51cdabe0cb11ebaedbb9
ahawkins94 <adamhawkins94@hotmail.co.uk>
2018-03-25 03:51:04 +0100

    added levelvariables file and made a start on getting the block prefabs to load properly

--- Functions/PathBlueprint.cs
+++ Functions/PathBlueprint.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PathBlueprint 
 {
+
+    List<int> list = new List<int>();
+    int[] array = new int[3];
+
+
+    void scrapeLevelBlocks(string location) {
+        Object[] levelBlocks = Resources.LoadAll("Prefabs/LevelBlocks", typeof(GameObject));
+        LevelVariables[] levelBlockVarList = new LevelVariables[levelBlocks.Length];
+        for (int i = 0; i < levelBlocks.Length; i++) {
+
+            // GameObject current = (GameObject) Instantiate(levelBlocks[i], new Vector3(0, 0, 0), Quaternion.identity);
+
+            // levelBlockVarList[i] = levelBlocks[i]
+        }
+    }
+
+
     public static void standardLevelBlocks(ref string randomStandardBlock){
 
         string[] standardBlockList = {
@@ -11,7 +28,9 @@ public class PathBlueprint
             "stone_flat2_15" 
             };
 
+
+
         int standardBlockListRange = standardBlockList.Length;
         randomStandardBlock = standardBlockList[Random.Range(0, standardBlockListRange)];
-    } 
+    }   
 }
